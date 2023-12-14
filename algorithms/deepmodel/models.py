67c21ebeb6ce298b17b0bdfb06e12d6a130300a7
Zetta <zeyangbai.rvo@gmail.com>
2023-12-14 10:51:37 +0800

    SRAFE feature and patch split

--- algorithms/deepmodel/models.py
+++ algorithms/deepmodel/models.py
@@ -5,4 +5,3 @@ resnet = resnet50(weights=None, num_classes=1)
 resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
 vit = vit_b_16(weights=None, num_classes=1)
 vit.conv_proj = nn.Conv2d(1, 768, kernel_size=(16, 16), stride=(16, 16))
-
