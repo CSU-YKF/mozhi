2645aedf4e922defc5c87d093b3df4fc03f0a6e3
Ruopu Chu <135697696+kyliancc@users.noreply.github.com>
2024-03-23 15:06:10 +0800

    Optimized exception catching method

--- algorithms/charinfo/cn_char_img2info.py
+++ algorithms/charinfo/cn_char_img2info.py
@@ -1,40 +1,24 @@
 from charinfo.cn_char_info import get_cn_char_info
 from charinfo.cn_char_recog import recog_cn_char
-import time
 
 
 class CNCharInfoGetter:
-    def __init__(self, access_token, retries=5, sleep_time=0.1):
+    def __init__(self, access_token):
         self.access_token = access_token
-        self.retries = retries
-        self.sleep_time = sleep_time
 
+    '''
+    获取信息失败时抛出 RuntimeError，需要异常处理
+    当输入图像未检测出有效汉字输出 None
+    '''
     def get_info(self, img_bin):
-        char = None
-        info = None
+        try:
+            char = recog_cn_char(img_bin, self.access_token)
+        except ValueError:
+            return None
 
-        for i in range(self.retries):
-            try:
-                char = recog_cn_char(img_bin, self.access_token)
-            except RuntimeError:
-                print('Getting output failed. Retrying...')
-                time.sleep(self.sleep_time)
-                continue
-            except ValueError:
-                return None
-
-            break
-
-        for i in range(self.retries):
-            try:
-                info = get_cn_char_info(char)
-            except RuntimeError:
-                print('Getting output failed. Retrying...')
-                time.sleep(self.sleep_time)
-                continue
-            except ValueError:
-                return None
-
-            break
+        try:
+            info = get_cn_char_info(char)
+        except ValueError:
+            return None
 
         return info
